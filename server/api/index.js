8188a5a087d8b132e78fc31f56c7a2fda276345c
Deep Prajapati <dprajap2@gmail.com>
2021-12-09 23:09:06 -0500

    Feat: set up post new recipe and put recipe routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const jsonData = require('../../data.json')
 
-console.log(jsonData)
+const foundRecipe = (name) => jsonData.recipes.find(recipe => recipe.name === name)
 
 // GET /recipes
 
@@ -9,7 +9,6 @@ router.get('/', function(req,res,next){
   try {
     const recipeNames = [];
     jsonData.recipes.forEach(recipe => recipeNames.push(recipe.name))
-    console.log('recipeNames', recipeNames)
     res.send(recipeNames).status(200)
   } catch (error) {
     next(error)
@@ -23,7 +22,7 @@ router.get('/', function(req,res,next){
 router.get('/details/:recipeName', function(req,res,next){
   try {
    const {recipeName} = req.params
-   const found = jsonData.recipes.find(recipe => recipe.name === recipeName)
+   const found = foundRecipe(recipeName)
    if(found){
    const details = {ingredients:found.ingredients, instructions:found.instructions}
     res.send(details).status(200);
@@ -35,6 +34,49 @@ router.get('/details/:recipeName', function(req,res,next){
     console.log('Error in the get/recipe/details/recipeName router', error)
   }
 })
+
+// POST to /recipes
+
+router.post('/', function(req,res,next){
+  try {
+    const {name, ingredients, instructions} = req.body
+    const newRecipe = {name,ingredients,instructions}
+    const found = foundRecipe(name)
+    if(!found){
+      jsonData.recipes.push(newRecipe);
+      res.status(201);
+    }else{
+      res.send("Recipe already exists").status(400);
+    }
+  } catch (error) {
+    next(error)
+    console.log("Error in the recipe post route",error)
+  }
+})
+
+
+// PUT to '/recipes'
+
+router.put('/', function(req,res,next){
+  try {
+    const {name, ingredients, instructions} = req.body
+    const found = foundRecipe(name);
+    if(found){
+      found.name = name
+      found.ingredients = ingredients;
+      found.instructions = instructions;
+      console.log('the new found', found)
+      res.status(204)
+    }else{
+      res.send("Recipe does not exist").status(404);
+    }
+
+  } catch (error) {
+    next(Error)
+    console.log("Error in the put recipes route", error)
+  }
+})
+
 
 
 // Error handling if client requests nonexisting route
